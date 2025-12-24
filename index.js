58e7ddffdc1a5d7df0cd444ac93b567083977c2b
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2025-12-23 21:32:35 -0500

    dom save

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Write your code here!
+const userInfo = {
+    username:"flatUser123",
+    description: "Hi, welcome to my page! just a new user of this website",
+    theme:"red"
+}
 
+function displayUser(userInfo){
+    const header = document.querySelector("#main")
+    header.textContent = userInfo.username + "'s Personal webpage"
+    header.classList = userInfo.theme
+
+    const description = document.createElement("p")
+    const body = document.querySelector("body")
+
+    description.textContent = userInfo.description
+	description.className = userInfo.theme
+
+    body.append(description)
+}
+
+displayUser(userInfo)
